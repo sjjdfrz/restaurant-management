2eb41f33904fd964a5fde68663cfa578c294824b
SajjadFarzane <sajjadfarzane1@gmail.com>
2023-08-21 20:33:36 +0330

    initial commit

---
+++ src/main/java/com/neshan/restuarantmanagement/RestuarantManagementApplication.java
@@ -0,0 +1,13 @@
+package com.neshan.restuarantmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestuarantManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestuarantManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neshan/restuarantmanagement/RestuarantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neshan.restuarantmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestuarantManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
