2f59b26709f3118230a4cfe3a731dabd8a756a7e
SajjadFarzane <sajjadfarzane1@gmail.com>
2023-08-22 19:24:25 +0330

    added exception handling.

---
+++ src/main/java/com/neshan/restuarantmanagement/exception/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.neshan.restuarantmanagement.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleNoSuchElementFoundException(NoSuchElementFoundException exc) {
+
+        ErrorResponse err = buildErrorResponse(exc);
+        return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleAllUncaughtException(Exception exc) {
+
+        ErrorResponse err = buildErrorResponse(exc);
+        return new ResponseEntity<>(err, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException exc) {
+
+        ErrorResponse err = buildErrorResponse(exc);
+
+        for (FieldError fieldError : exc.getBindingResult().getFieldErrors()) {
+            err.addValidationError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(err, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    public ErrorResponse buildErrorResponse(Exception exc) {
+
+        return ErrorResponse
+                .builder()
+                .message(exc.getMessage())
+                .timestamp(System.currentTimeMillis())
+                .build();
+    }
+}
---
+++ src/main/java/com/neshan/restuarantmanagement/exception/NoSuchElementFoundException.java
@@ -0,0 +1,8 @@
+package com.neshan.restuarantmanagement.exception;
+
+public class NoSuchElementFoundException extends RuntimeException {
+
+    public NoSuchElementFoundException(String message) {
+        super(message);
+    }
+}
